a3b234030646fe1857a86027fdbeef3f8604b0f5
Anton Boykevich <anton.b@itkungfu.ru>
2021-02-12 17:17:16 +0300

    deployments

--- cmd/service-discovery/config/config.go
+++ cmd/service-discovery/config/config.go
@@ -2,7 +2,7 @@ package config
 
 type Config struct {
 	InstanceName string `etcd:"/configs/service-discovery/{{SERVICE_DISCOVERY_INSTANCE}}/instance_name" default:"dev"`
-	ETCDTimeout  int    `etcd:"/configs/service-discovery/etcd_timeout" default:"10"`
+	ETCDTimeout  int    `etcd:"/configs/service-discovery/{{SERVICE_DISCOVERY_INSTANCE}}/etcd_timeout" default:"10"`
 	LogLevel     string `etcd:"/configs/service-discovery/{{SERVICE_DISCOVERY_INSTANCE}}/log_level,watcher" default:"debug"`
 	SentryDSN    string `etcd:"/configs/service-discovery/{{SERVICE_DISCOVERY_INSTANCE}}/sentry_dsn,watcher" default:""`
 }
